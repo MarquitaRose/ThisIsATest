8723cc2be01fc77523fb21518d8ed00e490adedd
mwalker2917 <mwalker2917@001LAB153010809.neisd.ad>
2015-10-20 10:08:19 -0500

    You're pretty cool, ya know?

---
+++ GitOctober/src/OctoBurr.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class OctoBurr {
+  public static void main (String args[]){
+	 Scanner booHoo = new Scanner(System.in);
+	 String ahaha = booHoo.nextLine();
+	 System.out.println(ahaha);
+	 
+  }
+}
