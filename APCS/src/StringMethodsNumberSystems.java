e52529878989793247b2142fcedf9c80877ac15e
Marquitarose <chocolatemusic02@gmail.com>
2015-10-29 21:21:41 -0500

    Added switch properties

--- APCS/src/StringMethodsNumberSystems.java
+++ APCS/src/StringMethodsNumberSystems.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+//: MANDATORY compareTo - equals - length - substring - indexOf
+//: OTHERS concat - charAt - replace - toUpperCase - toLowerCase - valueOf ? trim
 
 
 public class StringMethodsNumberSystems {
- public static void main(String args[]){
+	public static void main(String args[]){
+	System.out.println("");
+	choose();	
+	}
+	
+	public static void choose() {
+		Scanner options = new Scanner(System.in);
+
+		
+		switch(options.nextLine().toLowerCase()){
+		case "mandatory": {
+			switch(options.nextInt()){
+			case 1: System.out.println("Hay"); break;
+			case 2: System.out.println("Neigh"); break;
+			case 3: System.out.println("Sleigh"); break;
+			case 4: System.out.println("Yay"); break;
+				}  
+		}
+		break;
+		case "optional": {
+			switch(options.nextInt()){
+			case 1: System.out.println("more"); break;
+			case 2: System.out.println("Bore"); break;
+			case 3: System.out.println("chore"); break;
+			}
+		}
+		break;
+		default: System.out.println("Umpa loompas shall chase you now. :)"); break;
+		}
+		
+		
+		}
+	}
+
+	
+ /*public static void main(String args[]){
 	allTogetherNow();
 	subStrings();
 	indexTest();
@@ -37,6 +75,6 @@ public static void indexTest(){
 
 public static void lowerUpperCase(){
 	
-}
-}
+}*/
+
 
