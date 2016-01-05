71793f8854dcf5a24d4d6911c2df2ed022b35631
Marquitarose <chocolatemusic02@gmail.com>
2016-01-05 17:51:33 -0600

    Part 3/ 4

--- APCS/src/QAlgorithimsOne.java
+++ APCS/src/QAlgorithimsOne.java
@@ -1,206 +1,113 @@
 
-
     /////An algorithim is a step-by-step problem to a solution.\\\\\
-
                     /////Data Structures + Algorithms = Programs (Nikclaus Wirth)\\\\\
 
-
-    import java.util.Random;
-
+import java.util.Random;
 import java.util.Scanner;
-
-
-    
-
 
     public class QAlgorithimsOne{
 
-
-    
-
-
     public static void main(String args[]) 
-
     {
-
-        List3 array1 = new List3(60,100,200); 
-
-
-        array1.display();
-
-
-        array1.pause();
-
-
-        List3 array2 = new List3(100,100,999); 
-
-
-        array2.display();
-
-
-        array2.pause();
-
-
-        List3 array3 = new List3(200,10,19); 
-
-
-        array3.display();
-
-
-        array3.pause();
-
-
-        List3 array4 = new List3(40,500,505); 
-
-
-        array4.display();
-
-
-        array4.pause();
-
-
+        Scanner putin = new Scanner(System.in);
+        System.out.println("Enter the desired size of your list.");
+        int size = putin.nextInt();
+        System.out.println("What is the lowest number?");
+        int low = putin.nextInt();
+        System.out.println("What is the highest number?");
+        int high = putin.nextInt();
+        
+        List4 array = new List4(size, low, high);
+        array.display();
+        array.pause();
         System.out.println();
 
     }
 
-    }
+  }
 
 
-    class List3
+    class List4
 
     { 
 
         private int intArray[]; // stores array elements 
-
         private int size;  // number of elements in the array 
-
         private int minInt;  // smallest random integer 
-
         private int maxInt;  // largest random integer
 
 
-    public List3(int s) 
+/*    public List4(int s) 
 
     { 
 
     System.out.println("\nCONSTRUCTING NEW LIST OBJECT WITH DEFAULT VALUES"); 
-
     size = s; 
-
     intArray = new int[size]; 
 
     } 
+*/
 
-
-    public List3(int s, int n) 
-
+/*    public List4(int s, int n) 
     { 
-
     System.out.println("\nCONSTRUCTING NEW LIST OBJECT WITH SPECIFIED VALUES"); 
-
     size = s; 
-
     intArray = new int[size]; 
-
     for (int k = 0; k < size; k++) 
-
-
     intArray[k] = n; 
-
     } 
+*/
 
 
-    
-
-    public List3(int s, int min, int max) 
-
-
+    public List4(int s, int min, int max) 
     { 
-
-
     Random rndInt = new Random(12345);   
-
-
     minInt = min; 
-
-
     maxInt = max; 
-
-
     size = s; 
-
-
+    
     System.out.println("\nCONSTRUCTING LIST WITH VALUES in [" + minInt + ".." + maxInt + "] range"); 
-
-
     intArray = new int[size]; 
-
-
     int range = maxInt - minInt + 1; 
-
-
     for (int k = 0; k < size; k++) 
-
-
     intArray[k] = rndInt.nextInt(range) + minInt; 
-
-
     }
 
+    
+   
+    
+    
     public void assign() 
-
     { 
 
     System.out.println("\nASSIGNING RANDOM VALUES TO LIST OBJECT"); 
-
     Random rndInt = new Random(12345);
-
     for (int k = 0; k < size; k++) 
-
     intArray[k] = rndInt.nextInt(1000); 
-
-
     }
 
 
 
 
     public void display() 
-
     { 
-
         System.out.println("\nDISPLAYING ARRAY ELEMENTS");
-
         for (int k = 0; k < size; k++) 
-
         System.out.print(intArray[k] + "  ");
-
         System.out.println();
-
     } 
 
 
     
 
     public void pause() 
-
-
     {   
-
-
     Scanner input = new Scanner(System.in);  
-
-
     String dummy; 
 
-
     System.out.print("\nPress <Enter> to continue  ===>>  "); 
-
-
     dummy = input.nextLine(); 
-
-
-    }
+   }
 
       
 
