0c00415b3e50158c7b85cd1a4c6c0e2f8ee1799d
Marquitarose <chocolatemusic02@gmail.com>
2016-01-05 17:36:31 -0600

    Started to add Control Stuctures; we started a new unit in APCS so this is part one of the algorithims chapter

---
+++ APCS/src/CControlStructures.java
@@ -0,0 +1,15 @@
+/////Program Flow follows the exact sequence of listed program statements, unless directed otherwise\\\\ 
+                                  /////by a Java control structure.\\\\\
+
+/////Control provided by the sequence of program statements is called simple sequence.\\\\\
+/////Selection/conditional branching/decision making is when it is determined whether the program will
+                            /////go in a specific direction\\\\\
+/////Repetition does what it sounds like it does; it repeats part of a program\\\\\
+
+/////A conditional statement is a program expression, which evaluates to true or false. Most\\\\\ 
+/////conditional statements require a relational operator. All conditions must be placed inside\\\\\
+                          /////parentheses.\\\\\
+
+public class CControlStructures {
+
+}
